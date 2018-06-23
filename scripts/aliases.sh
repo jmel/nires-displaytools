0e1dbe47074770f41561169f8c651af984c9f330
Jason Melbourne <jason.melbourne@gmail.com>
2018-06-23 05:03:21 -1000

    updates to aliases for csh

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -9,22 +9,22 @@ alias dpv "dp v"
 alias dps "dp s"
 
 # alias quicklook scripts
-alias bp=$NIRES_PYTHON/bp.py
-alias bpv="bp v"
-alias bps="bp s"
+alias bp $NIRES_PYTHON/bp.py
+alias bpv "bp v"
+alias bps "bp s"
 
 # pdiff scripts
-alias pdiff=$NIRES_PYTHON/pdiff.py
-alias pdiffv="pdiff v"
-alias pdiffs="pdiff s"
+alias pdiff $NIRES_PYTHON/pdiff.py
+alias pdiffv "pdiff v"
+alias pdiffs "pdiff s"
 
 # ds9 display level scripts
-alias lindisp=$NIRES_PYTHON/lindisp.py
-alias lindispv="lindisp v"
-alias lindisps="lindisp s"
+alias lindisp $NIRES_PYTHON/lindisp.py
+alias lindispv "lindisp v"
+alias lindisps "lindisp s"
 
 # ds9 cursor scrips
-alias cu=$NIRES_PYTHON/cu.py
-alias cucent=$NIRES_SCRIPTS/cucent.sh
-alias cudel=$NIRES_SCRIPTS/cudel.sh
-alias cuslit=$NIRES_SCRIPTS/cuslit.sh
+alias cu $NIRES_PYTHON/cu.py
+alias cucent $NIRES_SCRIPTS/cucent.sh
+alias cudel $NIRES_SCRIPTS/cudel.sh
+alias cuslit $NIRES_SCRIPTS/cuslit.sh
