08771c99753c2927dc43a46f76062a13b432aa3a
JASON MELBOURNE <jason.melbourne@smartersorting.com>
2018-06-23 16:51:26 -1000

    updating aliases

--- scripts/aliases_keck.sh
+++ scripts/aliases_keck.sh
@@ -4,17 +4,17 @@ setenv NIRES_SCRIPTS /home/nireseng/projects/nires-displaytools/scripts
 setenv NIRES_PYTHON /home/nireseng/projects/nires-displaytools/nires/displaytools
 
 # ds9 display scripts
-alias dp $NIRES_PYTHON/dp.py
+alias dp "$NIRES_PYTHON/dp.py --d $DATADIR"
 alias dpv "dp v"
 alias dps "dp s"
 
 # alias quicklook scripts
-alias bp $NIRES_PYTHON/bp.py
+alias bp "$NIRES_PYTHON/bp.py --d $DATADIR"
 alias bpv "bp v"
 alias bps "bp s"
 
 # pdiff scripts
-alias pdiff $NIRES_PYTHON/pdiff.py
+alias pdiff "$NIRES_PYTHON/pdiff.py --d $DATADIR"
 alias pdiffv "pdiff v"
 alias pdiffs "pdiff s"
 
@@ -24,7 +24,7 @@ alias lindispv "lindisp v"
 alias lindisps "lindisp s"
 
 # ds9 cursor scrips
-alias cu $NIRES_PYTHON/cu.py
+alias cu "$NIRES_PYTHON/cu.py --d $DATADIR"
 alias cucent $NIRES_SCRIPTS/cucent.sh
 alias cudel $NIRES_SCRIPTS/cudel.sh
 alias cuslit $NIRES_SCRIPTS/cuslit.sh
