704ae4ccd9710e8a2a682127cf90984fe481f5d7
JASON MELBOURNE <jason.melbourne@smartersorting.com>
2018-06-23 15:33:52 -1000

    commenting out incomplete code

--- nires/scripts/wavelength_tools.py
+++ nires/scripts/wavelength_tools.py
@@ -3,6 +3,7 @@ def generate_wavelength_file_from_reg():
     with open("/Users/jlmelbourne/Projects/nires-displaytools/calibrations/tspec_wavelength.reg", "r") as file:
         for line in file:
             if "text" in line:
-                x_pos = line[]
-                y_pos =
-                lam =
+                pass
+                # x_pos = line[]
+                # y_pos =
+                # lam =
